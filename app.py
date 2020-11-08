621159ce913e15f743ef1a8cfce55b3be87fc269
MaxymSenkiv <maksym.senkiv.knm.2019@lpnu.ua>
2020-11-09 01:54:32 +0200

    '44444'

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/api/v1/hello-world-25')
+def start():
+    return 'Hello World 25'
+
+if __name__ == '__main__':
+    app.run()
