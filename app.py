5fa97a44b2f6347195a8cbd2313c00e7f0897f10
Bodya <bogdankalysh@Bodyas-MBP.Dlink>
2020-12-15 08:25:35 +0200

    Lab7 yaml fixed

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
-from wsgiref.simple_server import make_server
-app = Flask(__name__)
+from config import *
+
+from controllers.note_controller import *
+from controllers.user_controller import *
+
+db.create_all()
 
 
 @app.route('/')
@@ -8,15 +11,6 @@ def hello_world():
     return 'Hello World'
 
 
-@app.route('/api/v1/hello-world-25')
-def start():
-    return 'Hello World 25'
 
-
-with make_server('', 5000, app) as server:
-    print("Server is running")
-
-    server.serve_forever()
-
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run(debug = True)
